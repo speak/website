ff695c9388f34f9ffa3101d8e4dbfe99c7a94c6a
Will Barrett <will@tallgreentree.com>
2015-09-01 12:48:29 -0400

    remove profile reference

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ echo "Deploying to $1"
 ENVIRONMENT=$1
 
 DEFAULT="speak"
-PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=speak-${ENVIRONMENT}-site
 DIR=_site/
-aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
+aws s3 sync $DIR s3://$BUCKET/
