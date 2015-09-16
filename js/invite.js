546456729bc036c4ddea260f24379114ee8bcd9e
Luke Roberts <luke@sqwiggle.com>
2015-09-16 18:44:51 +0100

    wip

--- js/invite.js
+++ js/invite.js
@@ -1,17 +1,24 @@
+---
+---
 $(function(){
-  var $form = $('#invite-accept');
-  var action = $form.attr('action');
-  var invite = $form.attr('action').replace('/users', '/invite/');
+  var $inviteSignupForm = $('#invite-signup');
+  var $inviteSigninForm = $('#invite-signin');
+  var signupAction = $inviteSignupForm.attr('action');
+  var signinAction = $inviteSigninForm.attr('action');
+  var invite = $inviteSignupForm.attr('action').replace('/users', '/invite/');
   var code = getParam('code');
-  
+  var id;
+
   // load team name and email from bulldog
   $.ajax({
     type: 'GET',
     url: invite + code
   }).done(function(data, status, xhr){
+    id = data.id
+    console.log('id is: ' + id)
     // account for org invites without an email attached
     if (data.email) {
-      $form.find('input.email').val(data.email);
+      $inviteSignupForm.find('input.email').val(data.email);
     }
 
     // account for older invites without an org name attached
@@ -19,18 +26,18 @@ $(function(){
       $('.team-name').html('<strong>'+ data.organization_name +'</strong>');
     }
   });
-  
-  $form.on('submit', function(ev){
+
+  $inviteSignupForm.on('submit', function(ev){
     ev.preventDefault();
-    
+
     $.ajax({
       type: 'POST',
-      url: action,
+      url: signupAction,
       data: {
-        email: $form.find('input.email').val(),
-        first_name: $form.find('input.first-name').val(),
-        last_name: $form.find('input.last-name').val(),
-        password: $form.find('input.password').val(),
+        email: $inviteSignupForm.find('input.email').val(),
+        first_name: $inviteSignupForm.find('input.first-name').val(),
+        last_name: $inviteSignupForm.find('input.last-name').val(),
+        password: $inviteSignupForm.find('input.password').val(),
         invite_code: code
       }
     }).fail(function(xhr, status, error){
@@ -39,6 +46,46 @@ $(function(){
     }).done(function(data, status, xhr){
       $(".banner").hide();
       $('section').addClass('flipped');
+    });
+  });
+
+  $inviteSigninForm.on('submit', function(ev){
+    ev.preventDefault();
+
+    $.ajax({
+      type: 'POST',
+      url: signinAction,
+      data: {
+        email: $inviteSigninForm.find('input.email').val(),
+        password: $inviteSigninForm.find('input.password').val(),
+        device_id: 'account-area'
+      }
+    }).fail(function(xhr, status, error){
+      $(".banner").hide();
+      $(".banner.error").text(xhr.responseJSON.params.message).show();
+    }).done(function(data, status, xhr){
+      // $(".banner").hide();
+      // $('section').addClass('flipped');
+      console.log(data)
+      $.ajax({
+        type: "POST",
+        url: "{{site.manuel_url}}" + "/invite/accept",
+        processData: false,
+        contentType: 'application/json',
+        processData:false,
+        data: '{ "id": "' + id + '"}',
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader(
+            'Authorization',
+            'Basic ' + btoa(data.token + ':password'));
+        }
+      }).done(function(data, status, xhr){
+        console.log('invite accepted'); 
+      }).fail(function(xhr, status, error){
+        console.log("error")
+        console.log(error)
+        console.log(xhr)
+      });
     });
   });
 });
